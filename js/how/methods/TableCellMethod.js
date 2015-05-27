e2825f32a6e58a8915381cb169837de8f59ac9a5
Oliver Zheng <gitcommits@oliverzheng.com>
2015-05-26 22:42:17 -0700

    Fix table-cell method so it centers text properly.

--- js/how/methods/TableCellMethod.js
+++ js/how/methods/TableCellMethod.js
@@ -27,17 +27,25 @@ class TableCellMethod extends Method {
     verticalAlignment: Options.VerticalAlignment,
     browserSupport: Options.BrowserSupport
   ): { parent: ReactElement; middle: ?ReactElement; child: mixed; } {
-    var child = this.getContentWithDOM(content);
+    var child = this.getContentWithDOM(content, true /*requireBlock*/);
     var childStyles = child.props.style;
 
     var tableCellStyles = {};
     tableCellStyles.display = 'table-cell';
 
-    if (horizontalAlignment === Options.HorizontalAlignment.CENTER) {
-      childStyles.marginLeft = 'auto';
-      childStyles.marginRight = 'auto';
-    } else if (horizontalAlignment === Options.HorizontalAlignment.RIGHT) {
-      childStyles.marginLeft = 'auto';
+    if (content.text) {
+      if (horizontalAlignment === Options.HorizontalAlignment.CENTER) {
+        childStyles.textAlign = 'center';
+      } else if (horizontalAlignment === Options.HorizontalAlignment.RIGHT) {
+        childStyles.textAlign = 'right';
+      }
+    } else {
+      if (horizontalAlignment === Options.HorizontalAlignment.CENTER) {
+        childStyles.marginLeft = 'auto';
+        childStyles.marginRight = 'auto';
+      } else if (horizontalAlignment === Options.HorizontalAlignment.RIGHT) {
+        childStyles.marginLeft = 'auto';
+      }
     }
 
     if (verticalAlignment === Options.VerticalAlignment.MIDDLE) {
